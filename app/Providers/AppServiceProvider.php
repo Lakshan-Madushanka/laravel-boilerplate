2b84b301f8f26b559cc6ef0f124dba64152cdba1
Lakshan Madushanka <epmadushanka@gmail.com>
2023-04-27 15:35:45 +0530

    Report time consuming queries

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Exception;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +27,32 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::shouldBeStrict($this->app->environment(['local', 'testing']));
+    }
+
+    public function registerLocally(): void
+    {
+        if ( ! $this->app->isProduction()) {
+            $this->reportTimeConsumingQueries();
+        }
+    }
+
+    private function reportTimeConsumingQueries(): void
+    {
+        if ($this->app->runningInConsole()) {
+            return;
+        }
+
+        $maxTimeLimit = 500; // in milliseconds
+
+        DB::listen(static function (QueryExecuted $event) use ($maxTimeLimit): void {
+            if ($event->time > 500) {
+                throw new QueryException(
+                    $event->connectionName,
+                    $event->sql,
+                    $event->bindings,
+                    new Exception(message: "Individual database query exceeded {$maxTimeLimit}ms.")
+                );
+            }
+        });
     }
 }
